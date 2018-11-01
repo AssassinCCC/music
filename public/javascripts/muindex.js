e895707e0259f02fcc573ea1e19209667b3f5068
AssassinCC <382807334@qq.com>
2018-11-01 16:34:03 +0800

    更新了最后界面的优化代码

--- public/javascripts/muindex.js
+++ public/javascripts/muindex.js
@@ -20,7 +20,7 @@ var gainNode = ac[ac.createGain?"createGain":"createGainNode"]();
 gainNode.connect(ac.destination);
 
 var analyser = ac.createAnalyser();
-var size = 128;
+var size = 64;
 analyser.fftSize = size*2;
 analyser.connect(gainNode);
 
@@ -40,14 +40,17 @@ function random(m,n){
 }
 
 function getDots(){
+	Dots = []
 	for(var i = 0; i< size; i++){
 		var x = random(0,width);
 		var y =random(0,height);
-		var color = "rgb("+random(0,255)+","+random(0,255)+","+random(0,255)+")";
+		var color = "rgba("+random(0,255)+","+random(0,255)+","+random(0,255)+",0)";
 		Dots.push({
 			x:x,
 			y:y,
-			color:color
+			dx: random(1,4),
+			color:color,
+			cap:0
 		});
 	}
 }
@@ -73,21 +76,35 @@ window.onresize = resize;
 function draw(arr){
 	ctx.clearRect(0,0,width,height);
 	var w=width / size;
+	var cw = w*0.6;
+	var capH = (cw >10 ?10 :cw);
 		ctx.fillStyle = line;
 	for(var i = 0;i < size;i++){
+		var o = Dots[i];
 			if(draw.type=="column"){
 			var h = arr[i] / 256 * height;
-			ctx.fillRect(w * i,height - h,w * 0.6,h);
+			ctx.fillRect(w * i,height - h,cw,h);
+			ctx.fillRect(w * i,height - (o.cap+capH),cw,capH);
+			o.cap--;
+			if(o.cap<0){
+				o.cap=0;
+			}
+			if(h>0&&o.cap<h+40){
+				o.cap = h+40 > height - capH?height - capH:h+40;
+			}
+
 		}else if(draw.type == "dot"){
 			ctx.beginPath();
-			var o =Dots[i];
-			var r =arr[i]/256*50;
+
+			var r =10 + arr[i]/256*(height>width?width:height)/10;
 			ctx.arc(o.x,o.y,r,0,Math.PI*2,true);
 			var g = ctx.createRadialGradient(o.x,o.y,0,o.x,o.y,r);
 			g.addColorStop(0,"#fff");
 			g.addColorStop(1,o.color);
 			ctx.fillStyle=g;
 			ctx.fill();
+			o.x +=o.dx;
+			o.x=o.x>width?0:o.x;
 			//ctx.strokeStyle = "#fff";
 			//ctx.stroke();
 		}
